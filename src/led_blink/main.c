4c13aadb2941244ef70249f02b1d92a87ca2d61b
Birju Vachhani <birjuprincy@Birjus-MacBook-Pro.local>
2025-10-06 21:20:47 -0700

    Formatting

--- src/led_blink/main.c
+++ src/led_blink/main.c
@@ -1,35 +1,32 @@
-#include "RP2350.h"
-#include "hardware/regs/resets.h"
-#include "hardware/platform_defs.h"
-
 #include <stdint.h>
 
+#include "RP2350.h"
+#include "hardware/platform_defs.h"
+#include "hardware/regs/io_bank0.h"
+#include "hardware/regs/resets.h"
+
+#define ONBOARD_LED_GPIO 25
+
 void delay(int count) {
-    for (volatile int i = 0; i < count; i++){
-        __asm __volatile__("nop");
-    }
+  for (volatile int i = 0; i < count; i++) {
+    __asm __volatile__("nop");
+  }
 }
 
+int main() {
+  delay(1000);
 
-int main(){
-    delay(1000);
+  IO_BANK0->GPIO25_CTRL = IO_BANK0_GPIO25_CTRL_FUNCSEL_VALUE_SIOB_PROC_25;
+  PADS_BANK0->GPIO25 = 0x0;
+  delay(1000);
+  SIO->GPIO_OE_SET = (1 << ONBOARD_LED_GPIO);
+  delay(1000);
+  SIO->GPIO_OUT_SET = (1 << ONBOARD_LED_GPIO);
 
+  while (1) {
+    SIO->GPIO_OUT_XOR = (1 << ONBOARD_LED_GPIO);
+    delay(1000000);
+  }
 
-    // Reset and configure the onboard LED (GPIO25)
-    IO_BANK0->GPIO25_CTRL = 0x05; // Set GPIO25 to SIO function (for onboard LED)
-    PADS_BANK0->GPIO25 = 0x0;
-    delay(1000);
-    SIO->GPIO_OE_SET = (1 << 25); // Set GPIO25 as output
-    delay(1000);
-    SIO->GPIO_OUT_SET = (1 << 25); // Turn on LED
-
-    while (1)
-    {
-        SIO->GPIO_OUT_XOR = (1 << 25); // Toggle LED state
-        delay(1000000); // Simple delay
-    }
-    
-
-    return 0;
+  return 0;
 }
-

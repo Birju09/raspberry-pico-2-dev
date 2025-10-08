6e890dc34d35a4af498e4be37b4507ea48fe3228
Birju Vachhani <birjuprincy@Birjus-MacBook-Pro.local>
2025-10-07 18:52:07 -0700

    Fix warning

--- cmake/arm-toolchain.cmake
+++ cmake/arm-toolchain.cmake
@@ -7,13 +7,12 @@ SET(CMAKE_CROSSCOMPILING 1)
 SET(CMAKE_SYSTEM_NAME RaspberryPico)
 
 set(CPU_TYPE "-mcpu=cortex-m33")
-set(ARCH "-march=armv8-m.main")
 
-set(CMAKE_C_FLAGS "${CPU_TYPE} ${ARCH} -mthumb -ffunction-sections -nostartfiles --specs=nosys.specs -fdata-sections -Os -Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable")
-set(CMAKE_CXX_FLAGS "${CPU_TYPE} ${ARCH} -mthumb -ffunction-sections -nostartfiles --specs=nosys.specs -fdata-sections -Os -Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -fno-exceptions -fno-rtti")
-set(CMAKE_ASM_FLAGS "${CPU_TYPE} ${ARCH} -mthumb -ffunction-sections -nostartfiles --specs=nosys.specs -fdata-sections -Os -Wall -Wextra")
+set(CMAKE_C_FLAGS "${CPU_TYPE} -mthumb -ffunction-sections -nostartfiles --specs=nosys.specs -fdata-sections -Os -Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable")
+set(CMAKE_CXX_FLAGS "${CPU_TYPE} -mthumb -ffunction-sections -nostartfiles --specs=nosys.specs -fdata-sections -Os -Wall -Wextra -Wno-unused-parameter -Wno-unused-function -Wno-unused-variable -fno-exceptions -fno-rtti")
+set(CMAKE_ASM_FLAGS "${CPU_TYPE} -mthumb -ffunction-sections -nostartfiles --specs=nosys.specs -fdata-sections -Os -Wall -Wextra")
 
-set(CMAKE_EXE_LINKER_FLAGS "${CPU_TYPE} ${ARCH} -mthumb -Wl,--gc-sections -Wl,--print-memory-usage")
+set(CMAKE_EXE_LINKER_FLAGS "${CPU_TYPE} -mthumb -Wl,--gc-sections -Wl,--print-memory-usage")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
--- src/lib/led.c
+++ src/lib/led.c
@@ -1,11 +1,26 @@
 #include "led.h"
 #include "RP2350.h"
+#include <stddef.h>
 
 const uint32_t MAX_NUM_PINS = 30;
 
 void SetNthElement(uint32_t* base_ptr, uint32_t offset, uint32_t value) {
-    // dangerous stuff, but only way for to provide generic access
-    base_ptr[offset] = value;
+    // Ensure we have a valid pointer
+    if (base_ptr == NULL) {
+        return;
+    }
+
+    volatile uint32_t* reg_ptr = (volatile uint32_t*)base_ptr;
+    reg_ptr[offset] = value;
+}
+
+uint32_t GetControlRegIndexInIoBank(uint32_t gpio_pin) {
+    if (gpio_pin >= MAX_NUM_PINS) {
+        return 0xFFFFFFFF; // Invalid pin
+    }
+    // struct is laid out with status and control registers interleaved
+    volatile uint32_t* reg_ptr = (volatile uint32_t*)IO_BANK0;
+    return (2 * gpio_pin) + 1;
 }
 
 #define FUNC_SEL_GPIO 0x05
@@ -15,7 +30,7 @@ void led_init(uint32_t gpio_pin) {
     if (gpio_pin >= MAX_NUM_PINS) {
         return;
     }
-    SetNthElement((uint32_t*)IO_BANK0, (2 * gpio_pin) + 1, FUNC_SEL_GPIO);
+    SetNthElement((uint32_t*)IO_BANK0, GetControlRegIndexInIoBank(gpio_pin), FUNC_SEL_GPIO);
 
     SetNthElement((uint32_t*)&(PADS_BANK0->GPIO0), gpio_pin, 0x0);
     SIO->GPIO_OE_SET = (1 << gpio_pin);
